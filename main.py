fb9518d0211224d17e36fb4cc481f3348992a758
DorottyaKrisztics <dorka713@gmail.com>
2017-06-20 19:12:27 +0200

    set up flask based python application server

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    title = "Planets in the Star Wars universe"
+    return render_template("index.html", title=title)
+
+if __name__ == '__main__':
+    app.run()
