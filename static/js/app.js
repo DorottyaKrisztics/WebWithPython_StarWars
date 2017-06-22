69fe94a94c6b2822acd6fc4bdbeada181ae36f8e
DorottyaKrisztics <dorka713@gmail.com>
2017-06-22 22:42:18 +0200

    get data from swapi page and prepare data from planet array to create a row

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,25 @@
-$.getJSON('http://swapi.co/api/planets/1', function(response){
-    console.log(response['name'])
-});
+
+
+function processResults(planetArray) {
+    for (var i = 0; i < planetArray.length; i++) {
+        var planet = planetArray[i];
+        var row = createNewRow(planet);
+        // console.log(planet);
+        document.getElementById("planetTable").appendChild(row);
+    }
+}
+
+
+
+function getPlanetData() {
+    $.getJSON('http://swapi.co/api/planets/', function(response){    
+        // var firstPlanet = response['results'][0]['name'];
+        
+        processResults(response['results']);
+    });
+}
+
+
+
+$(document).ready(getPlanetData);
+
