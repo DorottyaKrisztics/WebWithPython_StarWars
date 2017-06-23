be59d1a1881e981ff63f4b124db94b987bd9ad68
DorottyaKrisztics <dorka713@gmail.com>
2017-06-23 11:45:02 +0200

    residents modal added to the html page and bootstrap modal event handler function is working

--- static/js/app.js
+++ static/js/app.js
@@ -52,6 +52,17 @@ function processResults(planetArray) {
 }
 
 
+function addModalEventHandler() {
+    $('#residentsModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('residents') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+    });
+}
+
 
 function getPlanetData() {
     $.getJSON('http://swapi.co/api/planets/', function(response){
@@ -60,6 +71,10 @@ function getPlanetData() {
     });
 }
 
+function main() {
+    getPlanetData();         
+    addModalEventHandler();
+}
 
-$(document).ready(getPlanetData);
+$(document).ready(main);
 
